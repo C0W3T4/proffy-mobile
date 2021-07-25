7a5d93ecb27b44bf3f42412f50005e8ca1ddb9f0
David Lopes <davidglcometa@gmail.com>
2021-07-25 19:58:02 +0100

    header component

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 import backIcon from '../../assets/images/icons/back.png';
 import logoImg from '../../assets/images/logo.png';
 
 import styles from './styles';
 
-function PageHeader() {
-  function handleGoBack() {
+interface PageHeaderProps {
+  title: string;
+}
 
+const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+  const { navigate } = useNavigation();
+  function handleGoBack() {
+    navigate('Landing');
   }
 
   return (
@@ -21,6 +27,8 @@ function PageHeader() {
 
         <Image source={logoImg} resizeMode="contain" />
       </View>
+
+      <Text style={styles.title}>{ title }</Text>
     </View>
   );
 }
